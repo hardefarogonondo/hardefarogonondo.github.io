b74055127b2d1b486733a8296b7a43eb32a9ca73
hardefarogonondo <hardefarogonondo@gmail.com>
2024-07-18 01:24:26 +0700

    Color palette updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,9 @@ module.exports = {
         "lavender-love": "#BA6AE3",
         "soft-cloud": "#F2F2F6",
         "charcoal": "#212121",
-        "misty-lilac": "#F0EDFC",
         "sky-light": "#C5D1F5",
+        "mint-green": "#98FF98",
+        "soft-teal": "#A4D3EE"
       },
       fontFamily: {
         montserrat: ["Montserrat", "sans-serif"],
